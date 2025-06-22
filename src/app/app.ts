b3a0422377394322b4008a60b39aaa1a6da74a9d
Yvan Grebe <yvan.grebe@gmail.com>
2025-06-22 19:44:10 +0100

    add interceptor

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { NavBar } from './components/nav-bar/nav-bar';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  standalone: true,
+  imports: [RouterOutlet, NavBar],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
